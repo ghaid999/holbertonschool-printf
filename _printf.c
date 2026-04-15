54ef08a9e9e062096225f425e2b25c5653eb7941
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-04-15 09:21:16 +0000

    Fix edge case for %

--- _printf.c
+++ _printf.c
@@ -37,6 +37,11 @@ int _printf(const char *format, ...)
 			count++;
 			i++;
 		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
 		else
 		{
 			write(1, &format[i], 1);
